91c4d6fee93abfc79ceb8097a2ed7b77dcd742bb
Nguyễn Đức Minh <minhndhe187040@fpt.edu.vn>
2025-03-27 12:30:53 +0700

    backlog Voucher

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.4.2"
 	id("io.spring.dependency-management") version "1.1.7"
-}
+    }
 
 group = "com.app"
 version = "0.0.1-SNAPSHOT"
--- src/main/java/com/app/food_booking_backend/model/dto/VoucherDTO.java
+++ src/main/java/com/app/food_booking_backend/model/dto/VoucherDTO.java
@@ -1,6 +1,6 @@
 package com.app.food_booking_backend.model.dto;
-import com.app.food_booking_backend.model.entity.enums.*;
 
+import com.app.food_booking_backend.model.entity.enums.*;
 
 import lombok.*;
 
@@ -15,7 +15,7 @@ public class VoucherDTO {
     private String id;
     private String code; 
     private String name;
-    private int discount;
+    private int discountPercentage;
     private int maxDiscountValue;
     private int minOrderValue;
     private LocalDateTime expiredAt;
@@ -23,4 +23,5 @@ public class VoucherDTO {
     private VoucherTypeEnum type;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt; 
+    private LocalDateTime deletedAt; 
 }
